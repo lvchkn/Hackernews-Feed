77aff5b9e0ba0a851f2481d3f0d0fe160c2bcd43
lvchkn <>
2023-05-20 01:01:21 +0200

    Method for seeding

--- Infrastructure/Repositories/DbContext.cs
+++ Infrastructure/Repositories/DbContext.cs
@@ -8,5 +8,53 @@ public class AppDbContext : DbContext
     public DbSet<Interest> Interests { get; set; }
     public DbSet<Tag> Tags { get; set; }
 
-    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) {}
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+    {
+        Database.EnsureCreated();
+    }
+
+    public void SeedData()
+    {
+        Stories.RemoveRange(Stories);
+        
+        Stories?.AddRange(
+            new Story()
+            {
+                By = "User1",
+                Title = "E Story1",
+                Score = 25,
+                Time = 1,
+            },
+            new Story()
+            {
+                By = "User2",
+                Title = "B Story",
+                Score = 44,
+                Time = 2,
+            },
+            new Story()
+            {
+                By = "User3",
+                Title = "A Story1",
+                Score = 79,
+                Time = 12,
+            },
+            new Story()
+            {
+                By = "User4",
+                Title = "C Story1",
+                Score = 900,
+                Time = 4,
+            },
+            new Story()
+            {
+                By = "User5",
+                Title = "B Story1",
+                Score = 250,
+                Time = 9,
+            }
+        );
+
+        SaveChanges();
+    }
 }
